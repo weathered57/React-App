72c53bd19323ea3c95246f727451ed6b72191628
weathered57 <a_canozdogru57@hotmail.com>
2020-04-25 19:38:30 +0300

    Added getCategories to CategoryActions

--- src/redux/actions/categoryActions.js
+++ src/redux/actions/categoryActions.js
@@ -3,3 +3,15 @@ import * as actionTypes from './actionTypes'
 export function changeCategory(category){
     return {type:actionTypes.CHANGE_CATEGORY,payload:category}
 }
+
+export function getCategoriesSuccess(categories){
+    return {type:actionTypes.GET_CATEGORIES_SUCCESS,payload:categories}
+}
+
+export function getCategories(){
+    return function(dispacth){
+        let url="https://localhost:5001/api/categories/getall"
+        return fetch(url).then(response => response.json())
+        .then(result=>dispacth(getCategoriesSuccess(result)));
+    }
+}
