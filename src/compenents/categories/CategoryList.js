16ebe4302c3af18ae6641d3a8a39ca26583bfba5
weathered57 <a_canozdogru57@hotmail.com>
2020-04-25 18:06:49 +0300

    Added redux to categoryList for currentCategory

--- src/compenents/categories/CategoryList.js
+++ src/compenents/categories/CategoryList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class CategoryList extends Component {
+ class CategoryList extends Component {
     render() {
         return (
             <div>
                 <h2>Categories</h2>
+        <h5>Secili Kategori : {this.props.currentCategory.categoryName}</h5>
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    return {
+        currentCategory:state.changeCategoryReducer
+    }
+}
+
+export default connect(mapStateToProps)(CategoryList);
