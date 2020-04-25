f88d00aebbca71f18f914e35d39e54cd67896d3c
weathered57 <a_canozdogru57@hotmail.com>
2020-04-25 16:55:20 +0300

    Added actions and reducers

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'
+import changeCategoryReducer from './changeCategoryReducer'
+
+const rootRecuder=combineReducers({
+    changeCategoryReducer
+})
+
+export default rootRecuder
