eb6287b78f966ecfe9176296277d172a5a506bd7
weathered57 <a_canozdogru57@hotmail.com>
2020-04-26 18:01:36 +0300

    selectedCategory

--- src/compenents/categories/CategoryList.js
+++ src/compenents/categories/CategoryList.js
@@ -1,48 +1,58 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {bindActionCreators} from 'redux'
-import * as categoryActions from '../../redux/actions/categoryActions'
-import { ListGroup, ListGroupItem } from 'reactstrap'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
-
- class CategoryList extends Component {
-
-    componentDidMount(){
-        this.props.actions.getCategories()
-    }
-
-    render() {
-        return (
-            <div>
-                <h3>Categories </h3>
-                <ListGroup>
-                    {
-                        this.props.categories.map(category => (
-                            <ListGroupItem key={category.categoryId}>
-                                {category.categoryName}
-                            </ListGroupItem>
-                        ))
-                    }
-                </ListGroup>
+class CategoryList extends Component {
+  componentDidMount() {
+    this.props.actions.getCategories();
+  }
+  selectedCategory = (category) => {
+    this.props.actions.currentCategory(category);
+  };
+  
+  render() {
+    return (
+      <div>
+        <h3>Categories </h3>
+        <ListGroup>
+          {this.props.categories.map((category) => (
+            <ListGroupItem
+              active= {category.categoryId===this.props.currentCategory.categoryId}
+              key={category.categoryId}
+              onClick={() => this.selectedCategory(category)}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
         <h5>Secili Kategori : {this.props.currentCategory.categoryName}</h5>
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-function mapStateToProps(state){
-    return {
-        currentCategory:state.changeCategoryReducer,
-        categories:state.categoryListReducer
-    }
+function mapStateToProps(state) {
+  return {
+    currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer,
+  };
 }
 
-function mapDispatchToProps(dispatch){
-    return{
-        actions:{
-            getCategories:bindActionCreators(categoryActions.getCategories,dispatch)
-        }
-    }
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      ),
+      currentCategory: bindActionCreators(
+        categoryActions.changeCategory,
+        dispatch
+      ),
+    },
+  };
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(CategoryList);
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,4 +1,4 @@
 export default{
-    currentCategory:{categoryName:"deneme"},
+    currentCategory:{},
     categories:[]
 }
