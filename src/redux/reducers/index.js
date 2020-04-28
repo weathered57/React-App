a0bbdbae4d092d5c2200b07ad466a114233238c3
weathered57 <a_canozdogru57@hotmail.com>
2020-04-28 21:32:21 +0300

    Added cartAction and cartReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux'
 import changeCategoryReducer from './changeCategoryReducer'
 import categoryListReducer from './categoryListReducer'
 import productListReducer from './productListReducer'
+import cartReducer from './cartReducer'
 
 const rootRecuder=combineReducers({
     changeCategoryReducer,
     categoryListReducer,
-    productListReducer
+    productListReducer,
+    cartReducer
 })
 
 export default rootRecuder
