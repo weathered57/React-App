a00c61454a2d1995e89fe81da3811d4774caa45b
weathered57 <a_canozdogru57@hotmail.com>
2020-05-26 01:30:35 +0300

    Editing Design

--- src/compenents/products/ProductList.js
+++ src/compenents/products/ProductList.js
@@ -4,16 +4,16 @@ import { bindActionCreators } from "redux";
 import * as productActions from "../../redux/actions/productActions";
 import * as cartAction from "../../redux/actions/cartAction";
 import { Badge, Table, Button } from "reactstrap";
-import alertify from 'alertifyjs'
+import alertify from "alertifyjs";
 
 class ProductList extends Component {
   componentDidMount() {
     this.props.actions.getProducts(this.props.currentCategory.categoryId);
   }
-  addToCart=(product)=>{
-     this.props.actions.addToCart({quantity:1,product});
-     alertify.success( product.productName+" sepete eklendi")
-  }
+  addToCart = (product) => {
+    this.props.actions.addToCart({ quantity: 1, product });
+    alertify.success(product.productName + " sepete eklendi");
+  };
 
   render() {
     return (
@@ -21,35 +21,28 @@ class ProductList extends Component {
         <h4>
           Product <Badge>{this.props.currentCategory.categoryName}</Badge>
         </h4>
-
-        <Table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Product Name</th>
-              <th>Unit Price</th>
-              <th>Quantity Per Unit</th>
-              <th>Units In Stock</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.products.map((product) => (
-              <tr key={product.productId}>
-                <th scope="row">{product.productId}</th>
-                <td>{product.productName}</td>
-                <td>{product.unitPrice}</td>
-                <td>{product.quantityPerUnit}</td>
-                <td>{product.unitsInStock}</td>
-                <td>
-                  <Button size="sm" color="outline-success" onClick={()=>this.addToCart(product)}>
+        <div className="row">
+          {this.props.products.map((product) => (
+            <div className="col-sm-4" style={{marginBottom:"10px"}}>
+              <div class="card">        
+                <div key={product.productId} class="card-body">
+                  <h5 class="card-title">{product.productName}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">
+                    {product.quantityPerUnit} - {product.unitsInStock}
+                  </h6>
+                  <p class="card-text">{product.unitPrice} ₺</p>
+                  <Button
+                    size="sm"
+                    color="outline-success"
+                    onClick={() => this.addToCart(product)}
+                  >
                     Ekle
                   </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
@@ -58,7 +51,7 @@ class ProductList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
-    products: state.productListReducer
+    products: state.productListReducer,
   };
 }
 
@@ -66,7 +59,7 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       getProducts: bindActionCreators(productActions.getProducts, dispatch),
-      addToCart: bindActionCreators(cartAction.addToCart, dispatch)
+      addToCart: bindActionCreators(cartAction.addToCart, dispatch),
     },
   };
 }
